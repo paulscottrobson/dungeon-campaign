f9f2565fa40beeedb20bf49f65494e333980f010
Paul Robson <paul@robsons.org.uk>
2017-04-01 12:15:04 +0100

    extracted files

---
+++ files/grab.sh
@@ -0,0 +1,10 @@
+
+java -jar AppleCommander-1.3.5.jar -g dungeon-campaign.dsk HELLO >files/hello.bin
+java -jar AppleCommander-1.3.5.jar -g dungeon-campaign.dsk "DUNGEON CAMPAIGN" >files/dungeon-campaign.bin
+java -jar AppleCommander-1.3.5.jar -g dungeon-campaign.dsk DRAGON0 >files/dragon0.bin
+java -jar AppleCommander-1.3.5.jar -g dungeon-campaign.dsk DRAGON1 >files/dragon1.bin
+java -jar AppleCommander-1.3.5.jar -g dungeon-campaign.dsk DUNGEON >files/dungeon.bin
+
+java -jar AppleCommander-1.3.5.jar -e dungeon-campaign.dsk HELLO >files/hello.bas
+java -jar AppleCommander-1.3.5.jar -e dungeon-campaign.dsk "DUNGEON CAMPAIGN" >files/dungeon-campaign.bas
+java -jar AppleCommander-1.3.5.jar -e dungeon-campaign.dsk DUNGEON >files/dungeon.bas
